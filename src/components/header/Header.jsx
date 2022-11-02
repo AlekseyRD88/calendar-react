c1ce08efe146333de77d45054f64ce92c27d41e3
= <neymegen50@gmail.com>
2022-11-02 14:34:43 +0200

    change week

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { getWeekStartDate, generateWeekRange } from '../../../src/utils/dateUtils.js';
 import './header.scss';
 
 const Header = () => {
+  const [weekStartDate, setWeekStartDate] = useState(new Date());
+  useEffect(() => {
+    setWeekStartDate(weekStartDate);
+  }, [])
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
   return (
     <header className="header">
       <button className="button create-event-btn">
@@ -10,10 +15,10 @@ const Header = () => {
       </button>
       <div className="navigation">
         <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="icon-button navigation__nav-icon" onClick={weekDates - 7}>
           <i className="fas fa-chevron-left"></i>
         </button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="icon-button navigation__nav-icon" onClick={weekDates + 7}>
           <i className="fas fa-chevron-right"></i>
         </button>
         <span className="navigation__displayed-month"></span>
