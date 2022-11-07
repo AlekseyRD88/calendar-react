2629e488ab34bdb3a0aefb032d1bc316cc5f9929
= <neymegen50@gmail.com>
2022-11-07 14:18:09 +0200

    message2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
-import { getWeekStartDate, generateWeekRange } from '../../../src/utils/dateUtils.js';
+import { getWeekStartDate, generateWeekRange, months } from '../../../src/utils/dateUtils.js';
 import './header.scss';
 
-const Header = () => {
-  const [weekStartDate, setWeekStartDate] = useState(new Date());
-  useEffect(() => {
-    setWeekStartDate(weekStartDate);
-  }, [])
-  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
-  let [week, setWeek] = useState(weekDates); 
-  useEffect(() => {
-    setWeek(week)
-  }, []);
-  let handleClickForward = () => {
-    week = moment().add(7, 'days')
-    setWeek(week);
-  };
-  let handleClickPast = () => {
-    week = moment().subtract(7, 'days')
-    setWeek(week);
-  };
+const Header = ({weekStartDate, setWeekStartDate}) => {
+  weekStartDate={weekStartDate};
+  setWeekStartDate={setWeekStartDate};
+  const month = new Date();
+  let currentMonth = months[month.getMonth()]
   return (
     <header className="header">
       <button className="button create-event-btn">
@@ -28,13 +15,13 @@ const Header = () => {
       </button>
       <div className="navigation">
         <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon" onClick={handleClickPast}>
+        <button className="icon-button navigation__nav-icon" onClick={() => setWeekStartDate(moment(weekStartDate).subtract(7, 'days'))}>
           <i className="fas fa-chevron-left"></i>
         </button>
-        <button className="icon-button navigation__nav-icon" onClick={handleClickForward}>
+        <button className="icon-button navigation__nav-icon" onClick={() => setWeekStartDate(moment(weekStartDate).add(7, 'days'))}>
           <i className="fas fa-chevron-right"></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">{currentMonth}</span>
       </div>
     </header>
   );
